43c530687280a30a9a5a802dee799fde001ecd37
Philipp Oppermann <dev@phil-opp.com>
2016-04-03 16:04:20 +0200

    Fix code

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,14 @@
 
 #![no_std]
 
-extern {
-	pub fn __rust_allocate(size: usize, align: usize) -> *mut u8 {}
-	pub fn __rust_usable_size(size: usize, align: usize) -> usize {}
-	pub fn __rust_deallocate(ptr: *mut u8, size: usize, align: usize) {}
-	pub fn __rust_reallocate(ptr: *mut u8, size: usize, new_size: usize,
-                                align: usize) -> *mut u8 {}
-	pub fn __rust_reallocate_inplace(ptr: *mut u8, size: usize,
-                                        new_size: usize, align: usize) -> usize {}
+extern "C" {
+    pub fn __rust_allocate(size: usize, align: usize) -> *mut u8;
+    pub fn __rust_usable_size(size: usize, align: usize) -> usize;
+    pub fn __rust_deallocate(ptr: *mut u8, size: usize, align: usize);
+    pub fn __rust_reallocate(ptr: *mut u8, size: usize, new_size: usize, align: usize) -> *mut u8;
+    pub fn __rust_reallocate_inplace(ptr: *mut u8,
+                                     size: usize,
+                                     new_size: usize,
+                                     align: usize)
+                                     -> usize;
 }
